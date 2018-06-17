e17968d9fc82c95af14943ea5b4259a6365ef9da
晋先森 <hi@jinxiansen.com>
2018-06-17 14:37:24 +0800

    fix.

--- VaporServer/Sources/App/Utility/Extension/String+Extension.swift
+++ VaporServer/Sources/App/Utility/Extension/String+Extension.swift
@@ -13,8 +13,8 @@ extension String {
     
     var isEmail : Bool {
         let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
-
-        let pred = NSPredicate(format: "SELF MATCHES \(outPutUnit)", pattern)
+        let str = "SELF MATCHES \(pattern)"
+        let pred = NSPredicate(format: str)
         let isMatch:Bool = pred.evaluate(with: self)
         return isMatch
     }
