2c8aaef66768bd05602240659a250ceeda84a26e
晋先森 <hi@jinxiansen.com>
2018-06-23 19:31:53 +0800

    add welcome.leaf

--- VaporServer/Sources/App/Controllers/HTMLController.swift
+++ VaporServer/Sources/App/Controllers/HTMLController.swift
@@ -39,7 +39,7 @@ extension HTMLController {
     
     func api(_ req: Request) throws -> Future<View> {
         
-        return try req.view().render("leaf/index")
+        return try req.view().render("leaf/Welcome")
     }
     
     //TODO: H
