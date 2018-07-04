3fc0f21160e27f41ba81914479103a3be0723827
晋先森 <hi@jinxiansen.com>
2018-07-04 16:28:37 +0800

    新增：支持分页爬拉勾并保存。

--- VaporServer/Sources/App/Controllers/CrawlerController.swift
+++ VaporServer/Sources/App/Controllers/CrawlerController.swift
@@ -10,14 +10,15 @@ import Vapor
 import SwiftSoup
 import FluentMySQL
 
-
-let LGHeader: HTTPHeaders = [
-    "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36     (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36",
-    "Referer":"https://www.lagou.com/jobs/list_ios"]
+private let crawlerInterval = TimeAmount.minutes(1) // 间隔1分钟
 
 class CrawlerController: RouteCollection {
     
     var timer: Scheduled<()>?
+    
+    var page = 1
+    var filterIndex: Int = 0
+    var result: [LGWorkItem]?
     
     func boot(router: Router) throws {
         
@@ -40,18 +41,35 @@ extension CrawlerController {
     
     func startTimer(_ req: Request) throws -> Future<Response> {
         
-        //未完，待续。
-        let timer = req.eventLoop.scheduleTask(in: TimeAmount.seconds(3)) {
-            _ = try self.crawlerLaGouWebHandler(req)
-            print(TimeManager.shared.currentTime())
+        guard self.timer == nil else {
+            return try ResponseJSON<Empty>(status: .error, message: "正在运行，请先调用 Cancel api").encode(for: req)
         }
-        self.timer = timer
-        return try ResponseJSON<Void>(status: .ok, message: "开始任务").encode(for: req)
+        
+         _ = try self.crawlerLaGouWebHandler(req)
+
+        return try ResponseJSON<Empty>(status: .ok, message: "开始任务").encode(for: req)
     }
     
     func cancelTimer(_ req: Request) throws -> Future<Response> {
         self.timer?.cancel()
-        return try ResponseJSON<Void>(status: .ok, message: "已取消").encode(for: req)
+        return try ResponseJSON<Empty>(status: .ok, message: "已取消").encode(for: req)
+    }
+    
+    func runRepeatTimer(_ req: Request) throws {
+        
+        if let result = self.result,result.count > 0 {
+            if self.filterIndex == result.count - 1 {
+                debugPrint("第\(page)页已爬完。\n")
+                page += 1
+                _ = try self.crawlerLaGouWebHandler(req)
+                return
+            }
+        }
+            
+        self.timer = req.eventLoop.scheduleTask(in: crawlerInterval) {
+            debugPrint("当前运行时间：" + TimeManager.shared.currentTime())
+            _ = try self.parseResultHandler(req)
+        }
     }
 }
 
@@ -61,7 +79,7 @@ extension CrawlerController {
         
         let urlStr = "http://swiftdoc.org"
         guard let url = URL(string: urlStr) else {
-            return try ResponseJSON<Void>(status: .error,
+            return try ResponseJSON<Empty>(status: .error,
                                           message: "URL 错误").encode(for: req)
         }
         let client = try req.client()
@@ -81,7 +99,7 @@ extension CrawlerController {
                     
                     let type = try? element.select("article[class='content']").select("h2").text()
                     guard let mainlist = try? element.select("ul[class='main-list'],li").select("a") else {
-                        return try ResponseJSON<Void>(status: .error,
+                        return try ResponseJSON<Empty>(status: .error,
                                                       message: "节点 错误").encode(for: req)
                     }
                     var titles = [String]()
@@ -101,17 +119,17 @@ extension CrawlerController {
     func crawlerQueryHandler(_ req: Request) throws -> Future<Response> {
         
         guard let urlStr = req.query[String.self, at: "url"] else {
-            return try ResponseJSON<Void>(status: .error,
+            return try ResponseJSON<Empty>(status: .error,
                                           message: "缺少 url 参数").encode(for: req)
         }
         
         guard let parse = req.query[String.self, at: "parse"] else {
-            return try ResponseJSON<Void>(status: .error,
+            return try ResponseJSON<Empty>(status: .error,
                                           message: "缺少 parse 参数").encode(for: req)
         }
         
         guard let url = URL(string: urlStr) else {
-            return try ResponseJSON<Void>(status: .error,
+            return try ResponseJSON<Empty>(status: .error,
                                           message: "url 错误").encode(for: req)
         }
         
@@ -148,14 +166,19 @@ extension CrawlerController {
     
     func crawlerLaGouWebHandler(_ req: Request) throws -> Future<Response> {
         
-        let city = "上海"
+        let city = "上海" //搜索城市
+        let key = "ios" //搜索关键词
         guard let urlStr = "https://www.lagou.com/jobs/positionAjax.json?city=\(city)&needAddtionalResult=false&isSchoolJob=0".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-            return try ResponseJSON<Void>(status: .error, message: "URL 转码错误").encode(for: req)
+            return try ResponseJSON<Empty>(status: .error, message: "URL 转码错误").encode(for: req)
         }
         
         guard let url = urlStr.convertToURL() else {
-            return try ResponseJSON<Void>(status: .error, message: "URL 错误").encode(for: req)
+            return try ResponseJSON<Empty>(status: .error, message: "URL 错误").encode(for: req)
         }
+        
+        let LGHeader: HTTPHeaders = [
+            "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36     (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36",
+            "Referer":"https://www.lagou.com/jobs/list_\(key)"]
         
         struct ReqBody: Content {
             var first: Bool = false //是否第一页
@@ -163,7 +186,7 @@ extension CrawlerController {
             var kd: String //搜索关键字
         }
         //第一页 first = true
-        let body = ReqBody(first: true, pn: 1, kd: "ios")
+        let body = ReqBody(first: page == 1 ? true: false, pn: page, kd: key)
         
         //构造请求体。
         var httpReq = HTTPRequest(method: .POST, url: url, headers: LGHeader)
@@ -174,58 +197,84 @@ extension CrawlerController {
         let postReq = Request(http: httpReq, using: req)
         try postReq.content.encode(body, as: .urlEncodedForm)
         
-        do {
-            return try req.client().send(postReq).flatMap(to: Response.self, { (clientResponse) in
+        return try req.client().send(postReq).flatMap(to: Response.self, { (clientResponse) in
+            
+            let jsonString = clientResponse.http.body.utf8String
+            let data = jsonString.convertToData()
+            let decode = JSONDecoder()
+            let lgItem = try decode.decode(LGResponseItem.self, from: data)
+            
+            if let result = lgItem.content?.positionResult?.result,result.count > 0 {
+                self.result = result
+                self.filterIndex = 0
                 
-                let jsonString = clientResponse.http.body.utf8String
-                let data = jsonString.convertToData()
-                let decode = JSONDecoder()
-                let lgItem = try decode.decode(LGResponseItem.self, from: data)
+                print("取到第\(self.page)页,\(result.count)条数据 -> 启动定时器：" + TimeManager.shared.currentTime())
+                try self.runRepeatTimer(req) //取到数据开始定时解析
                 
-                if let result = lgItem.content?.positionResult?.result {
-                   
-                    for index in 0..<result.count {
-                        let item = result[index]
-                        // I need sleep 120 seconds
-                        _ = LGWorkItem.query(on: req).filter(\.positionId == item.positionId).first().flatMap({ (exist) -> EventLoopFuture<LGWorkItem> in
-                            
-                            let fultureDetail = try self.requestDetailData(req, positionId: item.positionId)
-                            return fultureDetail.flatMap({ (detail) in
-                                
-                                if let exist = exist {
-                                    var updateItem = exist
-                                    updateItem.address = detail.address
-                                    updateItem.tag = detail.tag
-                                    updateItem.jobDesc = detail.jobDesc
-                                    
-                                    return updateItem.update(on: req).flatMap({ (update) in
-                                        debugPrint("已更新:\(TimeManager.shared.currentTime()) id:\(update.positionId))\n")
-                                        return req.eventLoop.newSucceededFuture(result: update)
-                                    })
-                                }else {
-                                    var saveItem = item
-                                    saveItem.address = detail.address
-                                    saveItem.tag = detail.tag
-                                    saveItem.jobDesc = detail.jobDesc
-                                    
-                                    return saveItem.save(on: req).flatMap({ (save) in
-                                        debugPrint("已保存:\(TimeManager.shared.currentTime()) id: \(save.positionId))\n")
-                                        return req.eventLoop.newSucceededFuture(result: save)
-                                    })
-                                }
-                            })
-                        })
-                    }
-                }
-                
-                return try ResponseJSON<String>(status: .ok, message: "目前爬了第一页", data: "共\(15) 条数据").encode(for: req)
-            })
-        } catch {
-            debugPrint("请求出错了： \(error)\n")
+                return try ResponseJSON<Empty>(status: .ok, message: "爬到\(result.count)条数据").encode(for: req)
+            }else {
+                self.timer?.cancel()
+            }
+            return try ResponseJSON<Empty>(status: .error, message: "没有数据了，任务已取消").encode(for: req)
+        })
+        
+    }
+    
+    
+    func parseResultHandler(_ req: Request) throws -> Future<LGWorkItem?> {
+        
+        guard let result = self.result,result.count > 0 else {
+            return req.eventLoop.newSucceededFuture(result: nil)
         }
         
-        return try ResponseJSON<String>(status: .ok, message: "已爬完第一页", data: "共爬\(15) 条数据").encode(for: req)
+        let item = result[filterIndex]
         
+        return LGWorkItem.query(on: req).filter(\.positionId == item.positionId).first().flatMap({ (exist)  in
+            
+            let fultureDetail = try self.requestDetailData(req, positionId: item.positionId)
+            return fultureDetail.flatMap({ (detail) in
+                
+                if var exist = exist {
+                    exist.address = detail.address
+                    exist.tag = detail.tag
+                    exist.jobDesc = detail.jobDesc
+
+                    self.filterIndex += 1
+                    try self.runRepeatTimer(req)
+                    return exist.update(on: req).flatMap({ (update) in
+                        debugPrint("已更新:\(TimeManager.shared.currentTime()) id:\(update.positionId))\n")
+                        return req.eventLoop.newSucceededFuture(result: update)
+                    })
+                }else {
+                    let newItem = LGWorkItem(
+                        id: nil, adWord: item.adWord, appShow: item.appShow,
+                        approve: item.approve, city: item.city, companyFullName: item.companyFullName,
+                        companyId: item.companyId, companyLogo: item.companyLogo, companyShortName: item.companyShortName,
+                        companySize: item.companySize, createTime: item.createTime, deliver: item.deliver,
+                        district: item.district, education: item.education, financeStage: item.financeStage,
+                        firstType: item.firstType, formatCreateTime: item.formatCreateTime, imState: item.imState,
+                        industryField: item.industryField, isSchoolJob: item.isSchoolJob, jobNature: item.jobNature,
+                        lastLogin: item.lastLogin, latitude: item.latitude, linestaion: item.linestaion,
+                        longitude:item.longitude, pcShow: item.pcShow, positionAdvantage:item.positionAdvantage,positionId:
+                        item.positionId, positionName: item.positionName, publisherId: item.publisherId,
+                        resumeProcessDay: item.resumeProcessDay, resumeProcessRate: item.resumeProcessRate,
+                        salary: item.salary, score: item.score, secondType: item.secondType,
+                        stationname: item.stationname, subwayline: item.subwayline, workYear: item.workYear,
+                        tag: detail.tag, jobDesc: detail.jobDesc, address: detail.address)
+                    
+                    let save = newItem.save(on: req)
+                    save.whenFailure({ (error) in
+                        debugPrint("保存失败: \(error)\n")
+                    })
+                    self.filterIndex += 1
+                    try self.runRepeatTimer(req)
+                    return save.flatMap({ (saveResult) in
+                        debugPrint("已保存:\(TimeManager.shared.currentTime()) id: \(saveResult.positionId))\n")
+                        return req.eventLoop.newSucceededFuture(result: saveResult)
+                    })
+                }
+            })
+        })
     }
     
     //TODO: 请求详情页
@@ -253,7 +302,7 @@ extension CrawlerController {
             
             let jobDesc = try document.select("dd[class='job_bt']").text()
             let address = try document.select("div[class='work_addr']").text().replacingOccurrences(of: "查看地图", with: "")
-            debugPrint("\(randomIP) 解析结果 = \(tag)\n\(jobDesc)\naddress: \(address)\n")
+            debugPrint("\(randomIP) 解析结果 = \(tag)\n\n")
             
             return req.eventLoop.newSucceededFuture(result: LGDetailItem(tag: tag, jobDesc: jobDesc, address: address))
         })
@@ -262,7 +311,7 @@ extension CrawlerController {
     
     func requestDetailDataHandler(_ req: Request) throws -> Future<Response> {
         guard let positionId = req.query[Int.self, at: "id"] else {
-            return try ResponseJSON<Void>(status: .error, message: " 缺少 id").encode(for: req)
+            return try ResponseJSON<Empty>(status: .error, message: " 缺少 id").encode(for: req)
         }
         return try requestDetailData(req, positionId: positionId).flatMap({ (item) in
             return try ResponseJSON<LGDetailItem>(data: item).encode(for: req)
