64a4b08d6fbb34c286f28b384fdff29573a93736
晋先森 <hi@jinxiansen.com>
2018-07-29 08:36:30 +0800

    use timeZone.

--- VaporServer/Sources/App/Utility/TimeManager.swift
+++ VaporServer/Sources/App/Utility/TimeManager.swift
@@ -15,7 +15,7 @@ struct TimeManager {
     
     init() {
         matter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        matter.locale = Locale(identifier: "zh_CN")
+        matter.timeZone = TimeZone.current
     }
     
     func currentTime() -> String {
