35e4198e1590e20a4a010d74451f49f09174af51
晋先森 <hi@jinxiansen.com>
2018-09-09 16:27:20 +0800

    fix `page` .

--- VaporServer/Sources/App/Utility/Middleware/PageViewMeddleware.swift
+++ VaporServer/Sources/App/Utility/Middleware/PageViewMeddleware.swift
@@ -11,10 +11,11 @@ import Vapor
 public final class PageViewMeddleware : Middleware {
     
     public func respond(to request: Request, chainingTo next: Responder) throws -> EventLoopFuture<Response> {
+        
         return try savePageView(request).flatMap({ pg in
             return try next.respond(to: request)
         })
-        
+    
     }
     
     func savePageView(_ req: Request) throws -> Future<PageView> {
@@ -24,13 +25,20 @@ public final class PageViewMeddleware : Middleware {
         let reqString = "\(method) \(path) \(TimeManager.currentTime()) \n"
         print(reqString)
         
-        let page = PageView(desc: req.http.description,
+        var body = ""
+        var desc = ""
+        if let subType = req.http.contentType?.subType, subType != "form-data" {
+            desc = req.http.description
+            body = req.http.body.description
+        }
+        
+        let page = PageView(desc: desc,
                             ip: req.http.remotePeer.description,
-                            body: req.http.body.description,
+                            body: body,
                             url: req.http.urlString)
         return page.save(on: req)
     }
- 
+    
     
 }
 
