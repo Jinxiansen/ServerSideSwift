e83d5ce2ea3a248cb0bf411a07ebb2c08643b287
晋先森 <hi@jinxiansen.com>
2018-09-30 00:26:00 +0800

    update TimeManager.

--- VaporServer/Sources/App/Utility/TimeManager.swift
+++ VaporServer/Sources/App/Utility/TimeManager.swift
@@ -22,6 +22,11 @@ struct TimeManager {
         return matter.string(from: Date())
     }
     
+}
+
+extension TimeManager {
+    
+    // Static func
     static func current() -> String {
         return self.shared.matter.string(from: Date())
     }
@@ -29,7 +34,6 @@ struct TimeManager {
     static func currentDate() -> TimeInterval {
         return Date().timeIntervalSince1970
     }
+    
 }
-
-
 
