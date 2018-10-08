d06395b4849f92592f3e262b35cbbace7b6a53a7
晋先森 <hi@jinxiansen.com>
2018-10-08 23:20:53 +0800

    use pipe.

--- VaporServer/Sources/App/Controllers/ProcessController.swift
+++ VaporServer/Sources/App/Controllers/ProcessController.swift
@@ -101,11 +101,19 @@ extension ProcessController {
         }
         
         let arcName = try VaporUtils.randomString()
-        
         let task = Process()
+        
         task.launchPath = VaporUtils.python3Path()
+        
         task.arguments = ["toImage.py",arcName,container.d ?? "1",imgPath ?? "",bgPath ?? ""]
+        
+        //        let outPipe = Pipe()
+        //        let errPipe = Pipe()
+        //        task.standardOutput = outPipe
+        //        task.standardError = errPipe
+        
         task.currentDirectoryPath = pyFileDir + "/convert"
+        
         task.terminationHandler = { proce in
             
             let filePath = pyFileDir + "/convert/out/\(arcName).jpg"
